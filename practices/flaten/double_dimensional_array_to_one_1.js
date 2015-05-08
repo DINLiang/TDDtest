d2ea666b798cc09d6a41c29d4f0706c2942edee0
DINLiang <15383332930@163.com>
2015-05-09 00:39:24 +0800

    完成第三个

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,19 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var array  = [];
+  for(var i = 0;i< collection.length;i++){
+    var new_array = collection[i];
+    if( new_array instanceof Array == true ){
+    for(var j = 0;j< new_array.length;j++){
+      array.push(new_array[j]);
+     }
+    }
+    else{
+      array.push(new_array);
+    }
+  }
+  return array;
 }
 
 //module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,30 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var array  = [];
+  var new_Array =[];
+  for(var i = 0;i< collection.length;i++){
+    var new_array = collection[i];
+    if( new_array instanceof Array == true ){
+      for(var j = 0;j< new_array.length;j++){
+        array.push(new_array[j]);
+      }
+    }
+    else{
+      array.push(new_array);
+    }
+  }
+  var length = array.length;
+  for(var i = 0;i < length; i++) {
+    for (var j = i + 1; j < length; j++) {
+      if (array[i] === array[j]) {
+        j = ++i;
+      }
+    }
+    new_Array.push(array[i]);
+  }
+  array = new_Array;
+  return array;
 }
 
-module.exports = double_to_one;
+//module.exports = double_to_one;
